daa4c4bc50ed0afe28dd1f343d707fd40421db16
Duncan McGreggor <duncan@mcgreggor.org>
2013-10-04 23:22:51 -0500

    Added more utility functions.

--- src/shankha/core.clj
+++ src/shankha/core.clj
@@ -1,15 +1,37 @@
 (ns shankha.core
-  (:require [clojure.java.shell :as shell])
+  (:require [clojure.java.shell :as shell]
+            [clojure.string :as string])
   (:gen-class))
 
 
-(defn sh
+(defn cmd
+  "Executables available in $PATH."
   [command & args]
   (print
     ((apply
        shell/sh
        (conj args command))
      :out)))
+
+(defn bi
+  "Built-in available shell commands."
+  [command & args]
+  (let [command (string/join \space (conj args command))]
+    (print
+      ((shell/sh "bash" :in command) :out))))
+
+(defn get-output [result]
+  (sort
+    (result :out)))
+
+(defn split-output [result]
+  (string/split (get-output result) #"\n"))
+
+(defn get-commands []
+  (split-output (bi "compgen -c")))
+
+(defn get-builtins []
+  (split-output (bi "compgen -b")))
 
 (defn -main
   ""
