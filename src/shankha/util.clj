457cffab6fcb12e0ffc4a55db1d708287174a2d4
Duncan McGreggor <duncan@mcgreggor.org>
2013-11-03 23:07:27 -0600

    Added a utility file.

---
+++ src/shankha/util.clj
@@ -0,0 +1,60 @@
+(ns shankha.util
+  (:require [clojure.java.shell :as shell]
+            [clojure.set :as set]
+            [clojure.string :as string]))
+
+
+(defn in?
+  "Given a sequence and a potential element of that sequence, determine if it
+  is, in fact, part of that sequence."
+  [sequence item]
+  (if (empty? sequence)
+    false
+    (reduce
+      #(or %1 %2)
+      (map
+        #(= %1 item)
+        sequence))))
+
+(defn not-in?
+  ""
+  [sequence item]
+  (not (in? sequence item)))
+
+(defn get-output [result]
+  (result :out))
+
+(defn split-output [result]
+  (string/split result #"\n"))
+
+(defn get-ns-keywords
+  ""
+  [name-space]
+  (into
+    #{}
+    (keys
+      (ns-map name-space))))
+
+(defn get-char-range
+  ""
+  [start stop]
+  (into
+    #{}
+    (map
+      (comp str char)
+      (range start stop))))
+
+(defn check-arg [arg]
+  "Let's support using keywords for flags."
+  ;; For example, the following are equivalent:
+  ;;   $ ls -alrth
+  ;; and:
+  ;;   => (ls "-alrth")
+  ;; and:
+  ;;   => (ls :alrth)
+  (cond
+    (keyword? arg) (str "-" (name arg))
+    :else arg))
+
+(defn check-args [args]
+  (map check-arg args))
