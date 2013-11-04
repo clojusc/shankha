3044ebcd8c60a51029363740d40416f1dd4d134d
Duncan McGreggor <duncan@mcgreggor.org>
2013-11-03 23:06:30 -0600

    Added another dep.
    Possibly for changing the cwd from inside the JVM.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.sun.jna/jna "3.0.9"]
                  [org.jruby.ext.posix/jna-posix "1.0.3"]]
   :aot [shankha.core]
   :main shankha.core
