a2b11a133bc499bae7b9002a7e2e11d1a76dcf0d
Bjorn Wickstrom <bjorstrom@users.noreply.github.com>
2016-10-06 10:13:22 -0500

    Added test and uber profiles.
    Updated dev to not use slamhound or testing plugins.

--- project.clj
+++ project.clj
@@ -36,10 +36,17 @@
                       " => "
                       end-color)))}
   :profiles {
+    :uber {
+      :aot :all}
+    :test {
+      :plugins [
+        [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+        [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]]
+      :test-selectors {
+      :default :unit
+      :unit :unit
+      :system :system
+      :integration :integration}}
     :dev {
-      :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                     [slamhound "1.5.5"]]
-      :plugins [[lein-kibit "0.1.2"]
-                [jonase/eastwood "0.2.3"]]
-      :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+      :dependencies [[org.clojure/tools.namespace "0.2.11"]]
       :source-paths ["dev-resources/src"]}})
